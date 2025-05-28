7df850940a80a8926845cfec19c6cffff07a03de
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-05-27 20:47:40 -0600

    Add sample parts, add inhousePartRepository to BootStrapData

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,10 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -28,34 +30,66 @@ public class BootStrapData implements CommandLineRunner {
     private final PartRepository partRepository;
     private final ProductRepository productRepository;
 
+    private final InhousePartRepository inhousePartRepository;
     private final OutsourcedPartRepository outsourcedPartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository, InhousePartRepository inhousePartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository=inhousePartRepository;
+
     }
 
     @Override
     public void run(String... args) throws Exception {
+        OutsourcedPart PA98= new OutsourcedPart();
+        OutsourcedPart EZ98= new OutsourcedPart();
+        OutsourcedPart ReStringZero= new OutsourcedPart();
+        OutsourcedPart SuperGrap= new OutsourcedPart();
+        InhousePart RacketBag = new InhousePart();
+        if (outsourcedPartRepository.count() == 0) {
+            PA98.setCompanyName("Babolat");
+            PA98.setName("Pure Aero 98");
+            PA98.setInv(5);
+            PA98.setPrice(289.99);
+            outsourcedPartRepository.save(PA98);
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+            EZ98.setCompanyName("Yonex");
+            EZ98.setName("EZONE 98");
+            EZ98.setInv(5);
+            EZ98.setPrice(304.99);
+            outsourcedPartRepository.save(EZ98);
+
+            ReStringZero.setCompanyName("ReString");
+            ReStringZero.setName("ReString Zero Tennis String Set");
+            ReStringZero.setInv(5);
+            ReStringZero.setPrice(14.00);
+            outsourcedPartRepository.save(ReStringZero);
+
+            SuperGrap.setCompanyName("Yonex");
+            SuperGrap.setName("Yonex Super Grap Overgrip");
+            SuperGrap.setInv(5);
+            SuperGrap.setPrice(8.00);
+            outsourcedPartRepository.save(SuperGrap);
+
+            RacketBag.setName("Racket Bag");
+            RacketBag.setInv(5);
+            RacketBag.setPrice(44.99);
+
+
+
+
         }
-
-        System.out.println(thePart.getCompanyName());
-        */
+//        OutsourcedPart thePart=null;
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+//        for(OutsourcedPart part:outsourcedParts){
+//            if(part.getName().equals("Pure Aero 98"))thePart=part;
+//        }
+//        System.out.println(thePart.getCompanyName());
+
+
+//        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
@@ -67,11 +101,11 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(unicycle);
         */
 
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
-        System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
-        System.out.println(partRepository.findAll());
+//        System.out.println("Started in Bootstrap");
+//        System.out.println("Number of Products"+productRepository.count());
+//        System.out.println(productRepository.findAll());
+//        System.out.println("Number of Parts"+partRepository.count());
+//        System.out.println(partRepository.findAll());
 
     }
 }
